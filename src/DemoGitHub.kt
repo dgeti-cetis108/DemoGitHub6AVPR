d5c6d2c046b2e23b4d258f4709d36882b3dcef15
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2022-03-14 10:26:17 -0700

    cambio a fuerzas
    cambio realizado en contra de mi voluntad, para mi seria mejor seguir como antes, copiando, pero alguien la cajeteo y por ello aqui andamos

---
+++ src/DemoGitHub.kt
@@ -0,0 +1,18 @@
+/**
+ * CETis108
+ * Semestre: Febrero-Julio 2022
+ * Asignatura: Desarrolla aplicaciones móviles
+ * Profesor: Bidkar Aragón Cárdenas
+ * Alumno(a): ?
+ * Fecha: 14/03/22 10:20
+ * Grupo: 6AV-PR
+ */
+
+
+fun main() {
+    println("Hola mundo de Git y GitHub")
+    println("Adios mundo de Git y GitHub")
+    println("Me caen gordos los copiones por lo " +
+            "que el profe nos hizo usar " +
+            "Git y GitHub")
+}
